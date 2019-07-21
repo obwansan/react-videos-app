a773cef3af2b3c1b6c9e360ec3d44bd2ee1e2331
Kevin O'Brien <kob123@hotmail.co.uk>
2019-07-21 16:36:32 +0100

    Updating state with fetched data

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,23 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    // 'youtube' is a preconfigured instance of axios
-    // '/search' is appended to the base URL. q:term is appended to the params.
-    // The GET request URL looks like this: https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&key=AIzaSyADjMF1SzxmoyDfGS1hXR9aWPBcYxMB7jY&q=buildings
-    youtube.get('/search', {
+  state = { videos: [] };
+
+  // The URL for a search for 'buildings' looks like this: https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&key=AIzaSyADjMF1SzxmoyDfGS1hXR9aWPBcYxMB7jY&q=buildings
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos
       </div>
     )
   }
